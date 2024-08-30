8ca7480e6049797814a6ede1e2179c4a1ca72d9c
afrazrajpoot <afrazrajpoot46@gmail.com>
2024-08-30 22:50:03 +0500

    change url

--- src/store/storeApi.js
+++ src/store/storeApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const storeApi = createApi({
   reducerPath: "storeApi",
-  baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:3000` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `http://sonduck-if7z.vercel.app` }),
   endpoints: (builder) => ({
     loginUser: builder.mutation({
       query: (data) => ({
